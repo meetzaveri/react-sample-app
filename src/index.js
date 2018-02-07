e857bee03914ac6bb860c26e94d540d84b059c82
Meet Zaveri <meethpatel96@gmail.com>
2018-02-07 19:00:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Doughnut from './containers/donut';
+import Donut from './containers/donut.js';
 import { ProgressBar } from './containers/progress-layer-donut';
+import PropTypes from 'prop-types';
 
-
-ReactDOM.render(<Doughnut hasEqualParts={true} doughnutsize="medium" doughnutparts={3} doughnutcolors={{c1:'#D1A917',c2:'#2C9DC2',c3:'#D12A6A',c4:'#535353',c5:'#AC6946'}} doughnutvalues={{p1:5,p2:20,p3:25,p4:30,p5:20}} completionrate={240}/>, document.getElementById('root'));
+class Doughnut extends Component {
+    render() {
+      return (
+        
+        <Donut hasEqualParts={this.props.equalparts} doughnutsize={this.props.size} doughnutparts={4} doughnutcolors={{c1:'#D1A917',c2:'#2C9DC2',c3:'#D12A6A',c4:'#535353',c5:'#AC6946'}} doughnutvalues={{p1:5,p2:20,p3:25,p4:25,p5:12}} completionrate={240} />
+        
+      );
+    }
+  }
+           
+Doughnut.propTypes = {
+    equalparts: PropTypes.bool,
+    size: PropTypes.string.isRequired,
+  };
+    
 export default Doughnut;
-registerServiceWorker();
+
